335e8887350084ad82501a1e2adfff60630f971f
Jakkavat (Bank) Deemak <jd@manaosoftware.com>
2018-07-31 15:09:49 +0700

    Add Object contructors.

---
+++ Playgroud/Defining Object Constructors/JS_OjectContructors.js
@@ -0,0 +1,14 @@
+function Person(name,surname) {
+    this.name = name;
+    this.surname = surname;
+    this.address = "";
+    this.email = "";
+    this.displayFullName = function () {
+        return this.name + " " + this.surname;
+    }
+}
+
+
+var johnSmith = new Person("John","Smith");
+
+var jakkavatDeemak = new Person("Jakkavat", "Deemak");
